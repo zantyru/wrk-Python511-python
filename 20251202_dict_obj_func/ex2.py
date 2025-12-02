64ee253aded2902cd5ab18c172721c52774e3db1
None <none@no.ne>
2025-12-02 20:57:46 +0300

    Добавляет код про словари, object (начало) и функции (начало)

---
+++ 20251202_dict_obj_func/ex2.py
@@ -0,0 +1,38 @@
+# Сотрудник
+employee = {
+    "фамилия": "Такойтов",
+    "имя": "Онсам",
+    "отчество": "Какойтович",
+    "доступные отделы": [
+        "пищеблок",
+        "бухгалтерия",
+        "бассейн",
+    ],
+    "параметры" : {
+        "должник ли": False,
+        "примерный семьянин ли": True,
+        "оптимист": True,
+    }
+}
+
+# Список сотрудников
+employees = []
+
+# Заполнение списка
+employees.append(employee)
+
+# employee_2 = employee  # Это неправильно, так будет копия ссылки
+
+from copy import deepcopy
+employee_2 = deepcopy(employee)  # Полная копия создаётся
+employee_2["фамилия"] = "Касаткин"
+employee_2["имя"] = "Джон"
+employee_2["отчество"] = "Доевич"
+employees.append(employee_2)
+
+# Распечатка
+for e in employees:
+    print()
+    for key, value in e.items():
+        print(key, " - ", value)
+
